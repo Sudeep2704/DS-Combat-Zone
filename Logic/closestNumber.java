57c144a3a27c6205dcb8a7e43aa3a0302e3a534d
sudeep2704 <ds.sudeep04@gmail.com>
2025-12-08 16:22:00 +0530

    add files

---
+++ Logic/closestNumber.java
@@ -0,0 +1,15 @@
+
+
+public class closestNumber {
+    public static void main(String args[]) {
+        int n = 13, m = 4;
+        int q = n / m;
+        System.out.println(q);
+
+
+
+
+
+    }
+}
+
